dd0f05d1516abc55bd50f5ab0e0e877f1bec4931
Dheeraj <31932986+kdrajr@users.noreply.github.com>
2025-10-25 18:39:17 +0530

    seperated aws_route_table.public creation and route for it

--- vpc.tf
+++ vpc.tf
@@ -74,10 +74,6 @@ resource "aws_subnet" "database" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
 
   tags = merge(
     var.public_route_table_tags,
@@ -87,6 +83,13 @@ resource "aws_route_table" "public" {
     }
   )
   
+}
+
+resource "aws_route" "public" {
+    route_table_id =  aws_route_table.public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+
 }
 
 resource "aws_route_table" "private" {
